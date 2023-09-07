858dfc27ec8a717ae925cd256109285de842fcd3
CodingCarnivore <143413977+CodingCarnivore@users.noreply.github.com>
2023-09-07 23:36:58 +0530

    Add files via upload

--- Task3/models.py
+++ Task3/models.py
@@ -3,13 +3,12 @@ class CaughtPokemon(models.Model):
     name=models.CharField(max_length=100)
     level=models.PositiveIntegerField(default=0)
 
-    sprite=models.ImageField(upload_to='pokemon_sprites/', blank=True, null=True) #even if no img is selected form gets validated due to blank
-    #if no image is provided field will store a null value in the database
+    sprite=models.ImageField(upload_to='pokemon_sprites/', blank=True, null=True) 
 
-    height=models.DecimalField(max_digits=5,decimal_places=2,null=True) #max_digits should always be greater than decimal_places
-    moves=models.JSONField(null=True) #stores an exact copy of the input text
+    height=models.DecimalField(max_digits=5,decimal_places=2,null=True) 
+    moves=models.JSONField(null=True) 
 
-    def __str__(self): #this method converts object into string and returns a name instead of an object
+    def __str__(self): 
         return self.name
 
 
